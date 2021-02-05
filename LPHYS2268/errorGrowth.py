b50bc1cabc91b9770c474c9184bc8f88f4010d2b
Francois Massonnet <francois.massonnet@uclouvain.be>
2021-02-05 13:29:05 +0100

    add error growth

---
+++ LPHYS2268/errorGrowth.py
@@ -0,0 +1,8 @@
+# to be executed on zenobe if the data is to be read
+from netCDF4 import Dataset
+
+root = "/home/acad/ucl-elic/fmasson/git/APPLICATE/data/"
+
+
+
+file = root + exp + str(member).zfill(3) + "_20110101_20121231.nc"
