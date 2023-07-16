5d015cda307ad1233780a74c323e5f4220292d2f
François Massonnet <francois.massonnet@uclouvain.be>
2023-07-16 14:51:53 +0200

    update mac

---
+++ LPHYS2268/project/download_SST.py
@@ -0,0 +1,22 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels-monthly-means',
+    {
+        'format': 'grib',
+        'product_type': 'monthly_averaged_reanalysis',
+        'variable': '2m_temperature',
+        'year': [
+            '2022',
+        ],
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'time': '00:00',
+    },
+    'download.grib')
