0ca93a167f969470eb98880e676d712f862b4d7a
François Massonnet <francois.massonnet@uclouvain.be>
2023-11-09 11:20:55 +0100

    Readme

--- LPHYS2163/2023/assemble.bash
+++ LPHYS2163/2023/assemble.bash
@@ -5,5 +5,10 @@ set -o errexit
 
 for tag in `cat listTime.txt`
 do
-    convert ./figs/z500_${tag}.png ./figs/t500_${tag}.png +append ./figs/all_${tag}.png
+    echo $tag
+    convert ./figs/z500_${tag}.png ./figs/t500_${tag}.png +append 1.png
+    convert ./figs/z1000_${tag}.png ./figs/t1000_${tag}.png +append 2.png
+    convert 1.png 2.png -append ./figs/all_${tag}.png
 done
+
+ffmpeg -framerate 30 -pattern_type glob -i './figs/all_*.png'  -c:v libx264 -pix_fmt yuv420p out.mp4
